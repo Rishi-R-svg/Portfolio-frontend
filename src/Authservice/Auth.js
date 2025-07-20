82ad768ee90d7dc605692b154d01125941aa01e6
unknown <you@example.com>
2025-07-20 15:11:46 +0530

    changed url

--- src/Authservice/Auth.js
+++ src/Authservice/Auth.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const sendData = (data)=> {
-    return axios.post(`${process.env.BACKEND_URL}/api/v1/contact`,data)
+    return axios.post(`https://portfolio-backend-qqlc.onrender.com/api/v1/contact`,data)
 }
 
 
